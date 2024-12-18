d42fb1bd04f6bf1585c812ca25bc4e4fc74b3598
celialctnm <celia.leichtnam@gmail.com>
2024-12-18 15:00:09 +0800

    function with api key gpt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import *
 import os
+from openai import OpenAI
 
 # convention à respecter
 # images, css, tout mettre dans le répertoire static sinon ça marche pas
@@ -15,6 +16,10 @@ UPLOAD_FOLDER = 'static/images'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# key for chatgpt
+client = OpenAI(
+    api_key="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")
+
 
 @app.route('/')
 def home():
@@ -26,9 +31,20 @@ def create_model():
     return model
 
 
+def chat_with_gpt(content):
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "user", "content": content}
+        ]
+    )
+    description = completion.choices[0].message.content
+    return description
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    audience = request.form.get('famille') or request.form.get('amis') or request.form.get('collegues')
+    audience = request.form.get('audience')
     if not audience:
         return redirect(url_for('index'))
 
@@ -43,9 +59,8 @@ def predict():
 
         # mise en place du model
         model = create_model()
-        print("Model")
 
-        description = "Generated description by model"
+        description = chat_with_gpt("Can you write a caption for my instagram post, my audience is my " + audience + ".The photo represents " + model)
         print(filepath)
     return render_template('result.html', audience=audience, image_path=filepath, description=description)
 
